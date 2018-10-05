3da3bd16c4da90e66b75a5406751e0817a430052
Christopher Pace <Christopher_pace@ymail.com>
2018-10-05 14:56:37 -0500

    finished

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -4,4 +4,6 @@ from django import forms
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    url = forms.URLField()
     text = forms.CharField(widget=forms.Textarea)
+
